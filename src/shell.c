1e6dff58bec1ae5e0e864b678fa6b87ce637aa5b
Vincent AUBRIOT <vincent38@users.noreply.github.com>
2020-02-05 00:11:46 +0100

    Up to ex5

--- src/shell.c
+++ src/shell.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "readcmd.h"
 #include "csapp.h"
 
@@ -29,8 +30,13 @@ int main()
 			continue;
 		}
 
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
+		if (l->in) {
+			printf("in: %s\n", l->in);
+		}
+
+		if (l->out) {
+			printf("out: %s\n", l->out);
+		}
 
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
@@ -40,6 +46,66 @@ int main()
 				printf("%s ", cmd[j]);
 			}
 			printf("\n");
+			// Quit shell
+			if (strcmp(cmd[0],"quit") == 0){
+				printf("exit\n");
+				exit(0);
+			} else {
+				// Fork process
+				pid_t process = fork();
+				int status;
+				if (process == -1){
+					// Error
+					printf("error while forking the processus.");
+					exit(1);
+				} else if (process == 0){
+					// Child
+					if (l->in) {
+						int fin = Open(l->in, O_RDONLY, 0);
+						if (fin < 0){
+							int stId = errno;
+							if (stId == EACCES){
+								printf("%s: Permission denied.", l->in);
+							} else if (stId == ENOENT){
+								printf("%s: No such file or directory.", l->in);
+							} else {
+								printf("%s: Unknown error.", l->in);
+							}
+						} else {
+							Dup2(fin, 0);
+							Close(fin);
+						}
+					}
+
+					if (l->out) {
+						int fout = Open(l->out, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+						if (fout < 0){
+	 						int stId = errno;
+							if (stId == EACCES){
+								printf("%s: Permission denied.", l->out);
+							} else if (stId == ENOENT){
+								printf("%s: No such file or directory.", l->out);
+							} else {
+								printf("%s: Unknown error.", l->out);
+							}						
+						} else {
+							Dup2(fout, 1);
+							Close(fout);
+						}
+						
+					}
+					if (execvp(cmd[0], cmd) == -1){
+						printf("%s: command not found\n", cmd[0]);
+						exit(-1);
+					}
+				} else {
+					// Father is awaiting
+					if (waitpid(process, &status, 0) == -1){
+						printf("error while catching the closed processus.");
+						exit(1);
+					}
+				}
+			}
 		}
 	}
 }
